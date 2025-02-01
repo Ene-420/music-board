868fd4f5af5fc047f13f921e622de948bb3b9645
Ene_Dimowo <78217379+Ene-420@users.noreply.github.com>
2025-02-01 13:03:17 +0000

    implement views

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const app = express();
-const musicRouter = require('./routes/MusicRouter');
-
-
-
+const mongoose = require("mongoose");
+const musicRouter = require('./routes/MusicRoutes');
+const PORT = 3000;
+const dbURL =
+    "mongodb+srv://Ene_420_VM:<db_password>@nodeclust.10gws.mongodb.net/?retryWrites=true&w=majority&appName=nodeCLUST";
+const passwd = "POp20";
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use("library/", musicRouter);
 
+app.get("/", (req, res) => {
+    res.redirect("/library");
+});
+app.use(musicRouter);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 
 
--- routes/MusicRoutes.js
+++ routes/MusicRoutes.js
@@ -3,8 +3,19 @@ const { Router } = require("express");
 const musicRouter = Router();
 
 
-musicRouter.get("/", (req, res) => {
-  res.render("index");
+musicRouter.get("/library", (req, res) => {
+  res.render("library", {library: []});
 });
 
+musicRouter.get("/artist", (req, res) => {
+  res.render("artists", { artists: [] }); 
+});
+
+musicRouter.get("/album", (req, res) => {
+  res.render("album", {album: []});
+});
+
+musicRouter.get("/search", (req, res) => {
+  //res.render("song");
+});
 module.exports = musicRouter;
